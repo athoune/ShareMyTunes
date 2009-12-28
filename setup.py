1d4368697ae64e3fb276f3f2f0ac2e916a9d3942
Mathieu Lecarme <mathieu@garambrogne.net>
2009-12-29 00:49:09 +0100

    data

--- setup.py
+++ setup.py
@@ -7,14 +7,19 @@ use_setuptools()
 from setuptools import setup
 
 setup(name='shareMyTunes',
-      version='0.1',
-      license='GPL-3',
-      description='iTunes db share',
-      author='Mathieu Lecarme',
-      author_email='mathieu@garambrogne.net',
-      url='http://github.com/athoune/ShareMyTunes',
-      packages=['shareMyTunes'],
-      package_dir={'': 'src/'},
-      #scripts=['src/toto'],
-      install_requires=["genshi", "whoosh", "mutagen", "pybonjour"]
-     )
+	version='0.1',
+	license='GPL-3',
+	description='iTunes db share',
+	author='Mathieu Lecarme',
+	author_email='mathieu@garambrogne.net',
+	url='http://github.com/athoune/ShareMyTunes',
+	packages=['shareMyTunes'],
+	package_dir={'': 'src/'},
+	package_data={'' : ['src/shareMyTunes/data/*.*']},
+	#scripts=['src/toto'],
+	install_requires=["genshi", "whoosh", "mutagen", "pybonjour"],
+	app=['src/shareMyTunes/server.py']
+)
+
+def demo():
+	print "beuha"
