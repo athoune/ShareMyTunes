08cc6bde16e95bb69300a9399dce56aa4ab6356b
Mathieu Lecarme <mathieu@garambrogne.net>
2010-10-02 20:20:59 +0200

    atom + media rss

--- src/shareMyTunes/opensearch.py
+++ src/shareMyTunes/opensearch.py
@@ -15,36 +15,64 @@ class OpenSearchWrapper(object):
 		self.query = query
 	def __iter__(self):
 		yield """
-			<?xml version="1.0" encoding="UTF-8"?>
-			 <rss version="2.0" 
-			      xmlns:opensearch="http://a9.com/-/spec/opensearch/1.1/"
-			      xmlns:atom="http://www.w3.org/2005/Atom">
-			   <channel>
-			     <title>Example.com Search: {query}</title>
-			     <link>{link}</link>
-			     <description>Search results for "{query}"</description>
-			     <opensearch:totalResults>{total}</opensearch:totalResults>
-			     <opensearch:startIndex>{startIndex}</opensearch:startIndex>
-			     <opensearch:itemsPerPage>{itemsPerPage}</opensearch:itemsPerPage>
-			     <atom:link rel="search" type="application/opensearchdescription+xml" href="http://example.com/opensearchdescription.xml"/>
-			     <opensearch:Query role="request" searchTerms="{query}" startPage="{startPage}" />""".format(
-					link='',
-					startIndex=0,
-					itemsPerPage=0,
-					startPage=0,
-					query=self.query,
-					total=len(self.search)
-			)
+<feed xmlns="http://www.w3.org/2005/Atom" 
+	xmlns:opensearch="http://a9.com/-/spec/opensearch/1.1/">
+	<title>Example.com Search: {query}</title> 
+	<link href="{link}"/>
+	<updated>2003-12-13T18:30:02Z</updated>
+	<author> 
+		<name>Share my tunes</name>
+	</author> 
+	<id>urn:uuid:60a76c80-d399-11d9-b93C-0003939e0af6</id>
+	<opensearch:totalResults>{total}</opensearch:totalResults>
+	<opensearch:startIndex>{startIndex}</opensearch:startIndex>
+	<opensearch:itemsPerPage>{itemsPerPage}</opensearch:itemsPerPage>
+	<opensearch:Query role="request" searchTerms="{query}" startPage="{startPage}" />
+	<!--
+	<link rel="alternate" href="http://example.com/New+York+History?pw=3" type="text/html"/>
+	<link rel="self" href="http://example.com/New+York+History?pw=3&amp;format=atom" type="application/atom+xml"/>
+	<link rel="first" href="http://example.com/New+York+History?pw=1&amp;format=atom" type="application/atom+xml"/>
+	<link rel="previous" href="http://example.com/New+York+History?pw=2&amp;format=atom" type="application/atom+xml"/>
+	<link rel="next" href="http://example.com/New+York+History?pw=4&amp;format=atom" type="application/atom+xml"/>
+	<link rel="last" href="http://example.com/New+York+History?pw=42299&amp;format=atom" type="application/atom+xml"/>
+	<link rel="search" type="application/opensearchdescription+xml" href="http://example.com/opensearchdescription.xml"/>
+	-->""".format(
+		link='',
+		startIndex=0,
+		itemsPerPage=0,
+		startPage=0,
+		query=self.query,
+		total=len(self.search)
+)
 		for s in self.search:
 			yield """
-			     <item>
-			       <title>{title}</title>
-			       <link>{link}</link>
-			       <description>
-			       </description>
-			     </item>
-		""".format(title = s['name'], link=s['location']).encode('utf8')
-		yield "</channel></rss>"
+	<entry xmlns="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/">
+		<title>{title}</title>
+		<link href="{link}"/>
+		<author>
+			<name>{author}</name>
+		</author>
+		<id>{id}</id>
+		<updated>2003-12-13T18:30:02Z</updated>
+		<content type="text">
+		</content>
+		<media:content
+			url="{link}"
+			bitrate="{bitrate}"
+		>
+			<media:title>{title}</media:title>
+			<media:category>{category}</media:category>
+		</media:content>
+	</entry>
+""".format(
+			title = s['name'],
+			link=s['location'],
+			id=s['trackId'],
+			author=s['artist'],
+			category=s['genre'],
+			bitrate=s.get('bitrate', 0)
+			)
+		yield "</feed>"
 		
 
 class Opensearchd(object):
@@ -52,13 +80,13 @@ class Opensearchd(object):
 		self.index = Index()
 	def notFound(self, start_response):
 		status = '404 NOT FOUND' # HTTP Status
-		headers = [('Content-type', 'text/plain')]#application/rss+xml
+		headers = [('Content-type', 'text/plain')]
 		start_response(status, headers)
 		return [' ']
 	def search(self, start_response, query):
 		search = self.index.query(unicode(query))
 		status = '200 OK' # HTTP Status
-		headers = [('Content-type', 'text/plain')]#application/rss+xml
+		headers = [('Content-type', 'text/plain')]#application/atom+xml
 		start_response(status, headers)
 		return OpenSearchWrapper(query, search)
 
